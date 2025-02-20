3296e36359ca85c42fc52a8ad0ab2876e65ec939
Stefan Pejcic <stefan@pejcic.rs>
2025-02-20 22:26:21 +0100

    Create update.sh

---
+++ docs/cli/update.sh
@@ -0,0 +1,18 @@
+# Update 
+
+`opencli update` command is used to check if new update is available and to update.
+
+Check if update is available:
+```bash
+opencli update --check
+```
+
+Start update immediately if `autoupdate` or `autopatch` is enabled:
+```bash
+opencli update
+```
+
+Start update immediately:
+```bash
+opencli update --force
+```
